0c426a8e45b3081a2630689fcfd50b656613c407
Ghostdoescode <akanjimichael158@gmail.com>
2022-09-08 22:05:23 +0100

    prog

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+/**
+* main -print the string in the put function
+* Description: using the main function
+* this prints "Programming is like building amultilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+         puts("\"Programming is like building a multilingual puzzle");
+         return (0);
+}
